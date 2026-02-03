87cca06cf865955d5c7ac819b35c2fac25850cbb
kashish25csu107-debug <kashish25csu107@ncuindia.edu>
2026-02-03 11:15:26 +0530

    Delete FOCP 2 Lab Manual directory

--- FOCP 2 Lab Manual/prog1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-    int a =10;
-    float b =12.5;
-    char c='z';
-    cout << "The value of a=" << a <<endl << "The value of b ="<< b <<endl<< "The value of c =" <<c <<endl;
-    return 0;
-}
