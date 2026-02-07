4e704ab4196480eaf78074a51929d29dc16b98b1
kashish25csu107-debug <kashish25csu107@ncuindia.edu>
2026-02-07 13:34:36 +0530

    Add files via upload

---
+++ q21.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1 ;i<=1; i++){
+        cout << "********\n*      *\n*      *\n*      *\n*      *\n*      *\n*      *\n********" << endl;
+    }
+    return 0;
+
+
+}
