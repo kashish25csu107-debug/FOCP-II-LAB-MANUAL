d950e06a34e625cb850022b862141003dba6193a
kashish25csu107-debug <kashish25csu107@ncuindia.edu>
2026-04-28 10:55:37 +0530

    Add files via upload

---
+++ erase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ #include <set>
+  using namespace std; 
+  int main() { 
+    set<int> s; 
+    
+    s.insert(10);
+     s.insert(20);
+     s.insert(30);
+      if(s.find(20) != s.end()) cout << "Found\n";
+       s.erase(20); 
+       for(int x : s) cout << x << " ";
+ } 
